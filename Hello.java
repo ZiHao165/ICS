404da28a82f593db05b9d541ab80170ded8fd7df
ZiHao165 <83368057+ZiHao165@users.noreply.github.com>
2021-04-28 14:54:45 -0400

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Today is April 28, 2021");
+		System.out.println("We are learning about Java programming");
+	}
+}
